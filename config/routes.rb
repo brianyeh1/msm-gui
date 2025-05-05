bc6786b6b87c3e0ca8b85125e1ab638284a4ddb4
brianyeh1 <nyeh@chicagobooth.edu>
2025-05-05 05:57:32 +0000

    movie all

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
 
   get("/movies", { :controller => "movies", :action => "index" })
+  post("/movies/create_movie", { :controller => "movies", :action => "create" })
+  post("/movies/update_movie/:path_id", { :controller => "movies", :action => "update" })
+  get("/movies/delete_movie/:path_id", { :controller => "movies", :action => "delete" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   
   get("/actors", { :controller => "actors", :action => "index" })
